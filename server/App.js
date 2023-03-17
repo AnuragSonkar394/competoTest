64ab7bd5e23a3411e8d4d008bb167d00905f48ba
unknown <anuragsonkar737@gmail.com>
2023-03-18 03:26:02 +0530

    some required changes

--- server/App.js
+++ server/App.js
@@ -11,9 +11,16 @@ require('./db/conn');
 app.use(express.json());
 //const User = require('./model/userSchema');
 app.use(require('./routers/auth'));
-const PORT = process.env.PORT;
+
 
 console.log("hello Anurag");
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+  const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`running on port nso $(PORT)`);
 })
