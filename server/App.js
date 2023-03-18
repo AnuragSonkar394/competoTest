3ae2d49bc0391d087bcb23dd9368bb5c30b8362c
unknown <anuragsonkar737@gmail.com>
2023-03-19 01:15:41 +0530

    some required changes

--- server/App.js
+++ server/App.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const mongoose= require('mongoose');
 const express = require('express');
+const path= require('path');
 const app = express();
 dotenv.config({ path: './config.env'});
 require('./db/conn');
@@ -16,9 +17,9 @@ app.use(require('./routers/auth'));
 console.log("hello Anurag");
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, "./client/build")));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, "./client/build/index.html")));
   }
   const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
